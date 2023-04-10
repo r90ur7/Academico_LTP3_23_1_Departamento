ad4ec5557536e77aacf73300893f1d23e5e321a1
Tamires Paiva <tamires.paiva@outlook.com>
2023-04-09 22:51:43 -0300

    Adicionar arquivos de projeto.

---
+++ Academico/Controllers/DepartamentoController.cs
@@ -0,0 +1,71 @@
+﻿using Academico.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Academico.Controllers
+{
+    public class DepartamentoController : Controller
+    {
+        private static IList<Departamento> departamentos = new List<Departamento>()
+        {
+            new Departamento
+            {
+                DepartamentoId=1,
+                Nome = "Hogwarts",
+            },
+            new Departamento
+            {
+                DepartamentoId=2,
+                Nome = "Mansão X",
+            }
+        };
+        public IActionResult Index()
+        {
+            return View(departamentos);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Departamento departamento)
+        {
+            departamento.DepartamentoId = departamentos.Select(i => i.DepartamentoId).Max() + 1;
+            departamentos.Add(departamento);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(long id)
+        {
+            return View(departamentos.Where(i => i.DepartamentoId == id).First());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Departamento departamento)
+        {
+            departamentos.Remove(departamentos.Where(i => i.DepartamentoId == departamento.DepartamentoId).First());
+            departamentos.Add(departamento);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(long id) 
+        {
+            return View(departamentos.Where(i => i.DepartamentoId == id).First());
+        }
+
+        public IActionResult Delete(long id)
+        {
+            return View(departamentos.Where(i => i.DepartamentoId == id).First());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Departamento departamento)
+        {
+            departamentos.Remove(departamentos.Where(i => i.DepartamentoId == departamento.DepartamentoId).First());
+            return RedirectToAction("Index");
+        }
+    }
+}
+
+
